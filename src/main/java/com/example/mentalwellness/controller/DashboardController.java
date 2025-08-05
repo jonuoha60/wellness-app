700caf74b78945884f157460dd8b331ef179e4f9
Ikenna Onuoha <onuohaikenna13@gmail.com>
2025-08-05 08:11:33 -0400

    dashboard update

--- src/main/java/com/example/mentalwellness/controller/DashboardController.java
+++ src/main/java/com/example/mentalwellness/controller/DashboardController.java
@@ -1,13 +1,19 @@
 package com.example.mentalwellness.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.mentalwellness.model.User;
 
 @Controller
 public class DashboardController {
 
-    @GetMapping("/dashboard")
-    public String dashboard(){
-        return "dashboard";
-    }
+@GetMapping("/dashboard")
+public String dashboard(Model model, Authentication authentication) {
+    String email = authentication.getName(); 
+    model.addAttribute("email", email);
+    return "dashboard";
+}
 }
